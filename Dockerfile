6d18618df8d46068c94f2681c483bd5e83d93709
XternA <xternia@gmail.com>
2024-05-21 15:38:44 +0000

    Shift entire build process to Dockerfile, cleanup and moved image base to debian

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,35 @@
-FROM ubuntu:rolling
+FROM debian:stable-slim
 
 LABEL author="XternA"
 LABEL description="Unofficial docker image of GaGaNode."
 
-RUN apt update; apt install -y sudo curl tar ca-certificates
-
-RUN useradd -ms /bin/bash ubuntu; usermod -aG sudo ubuntu
-RUN echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-
+RUN apt update; apt install -y --no-install-recommends sudo curl tar ca-certificates; rm -rf /var/lib/apt/lists/*
 RUN rm -rf /sbin/initctl && ln -s /sbin/initctl.distrib /sbin/initctl
 
 WORKDIR /app
+
+RUN case "$(uname -m)" in \
+        x86_64) \
+            TYPE=60; \
+            ;; \
+        arm64|aarch64) \
+            TYPE=61; \
+            ;; \
+        armv8l|armv7l|armv6l) \
+            TYPE=72; \
+            ;; \
+        *) \
+            echo "CPU type not supported."; \
+            exit 1; \
+            ;; \
+    esac; \
+    LINK="https://assets.coreservice.io/public/package/$TYPE/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz"; \
+    FILENAME="app-linux.tar.gz"; \
+    curl -L -o $FILENAME $LINK; \
+    tar -xzvf $FILENAME; \
+    rm -f $FILENAME
+
 COPY . .
 
-RUN sudo chmod 777 ./run.sh
+RUN chmod 777 ./run.sh
 CMD ./run.sh; sleep infinity
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 echo '
    ____          ____         _   _             _
@@ -8,70 +8,22 @@ echo '
   \____| \__,_| \____| \__,_||_| \_| \___/  \__,_| \___|
 '
 
-if [ -d ./apphub-linux* ]; then
-    cd ./apphub-linux* || exit 1
+cd ./apphub-linux* || exit 1
 
-    sudo ./apphub service start
-    sleep 60
-    sudo ./apphub status
-    sudo ./apphub log
-    sudo ./apps/gaganode/gaganode log
-    echo
-    echo "Token: $TOKEN"
-else
-    CPU_ARCH=$(uname -m)
+sudo ./apphub service remove
+sudo ./apphub service install
+sudo ./apphub service start
 
-    case "$CPU_ARCH" in
-        x86_64)
-            FILENAME="app-linux-amd64.tar.gz"
-            TYPE=60
-            ;;
-        arm64|aarch64)
-            FILENAME="app-linux-arm64.tar.gz"
-            TYPE=61
-            ;;
-        i386)
-            FILENAME="app-linux-386.tar.gz"
-            TYPE=70
-            ;;
-        armv8l|armv7l|armv6l)
-            FILENAME="app-linux-arm32.tar.gz"
-            TYPE=72
-            ;;
-        *)
-            echo "Your CPU type is not supported."
-            exit 1
-            ;;
-    esac
+echo
+echo "Bootstrapping 30s timeout..."
+sleep 30
 
-    LINK="https://assets.coreservice.io/public/package/$TYPE/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz"
-
-    echo "CPU Arch: $CPU_ARCH"
-    echo "Download link: $LINK"
-    echo "Binary: $FILENAME"
-    echo
-    echo "Downloading binary..."
-
-    sudo curl -o $FILENAME $LINK
-    sudo tar -zxf $FILENAME
-    sudo rm -f $FILENAME
-    cd ./apphub-linux* || exit 1
-
-    sudo ./apphub service remove
-    sudo ./apphub service install
-    sudo ./apphub service start
-
-    echo
-    echo "Bootstrapping 30s timeout..."
-    sleep 30
-
-    sudo ./apphub status
-    sudo ./apps/gaganode/gaganode config set --token="$TOKEN"
-    sudo ./apphub restart
-    sleep 15
-    sudo ./apphub status
-    sudo ./apphub log
-    sudo ./apps/gaganode/gaganode log
-    echo
-    echo "Token: $TOKEN"
-fi
+sudo ./apphub status
+sudo ./apps/gaganode/gaganode config set --token="$TOKEN"
+sudo ./apphub restart
+sleep 15
+sudo ./apphub status
+sudo ./apphub log
+sudo ./apps/gaganode/gaganode log
+echo
+echo "Token: $TOKEN"
